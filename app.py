3b3de65622f4afeeea918033fa364d279ac88caa
jgonz22 <jgonz22@iastate.edu>
2023-09-26 20:53:59 -0500

    Added mysql connection

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template, request
+import mysql.connector
 app = Flask(__name__)
 
-recipes = [
-    {"id": 1, "title" : "Sancocho", "description": "Sabe rico"},
-    {"id": 2, "title" : "Arroz con Gandules", "description": "bueno con lechon"},
-    {"id": 3, "title" : "Pasteles", "description": "Bueno para la navidad"},
-]
+# recipes = [
+#     {"id": 1, "title" : "Sancocho", "description": "Sabe rico"},
+#     {"id": 2, "title" : "Arroz con Gandules", "description": "bueno con lechon"},
+#     {"id": 3, "title" : "Pasteles", "description": "Bueno para la navidad"},
+# ]
+
+# Configure your MySQL database connection
+db_config = {
+    "host": "database-1.cz1imqcncgge.us-east-2.rds.amazonaws.com",
+    "user": "feljon",
+    "password": "feljonisu822",
+    "database": "database-1",
+}
 
 @app.route('/')
 def home():
-    return render_template('index.html', recipes = recipes)
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+
+    # Fetch data from the Recipes table
+    cursor.execute("SELECT * FROM Recipes")
+    recipes = cursor.fetchall()
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    return render_template('home.html', recipes=recipes)
 
 @app.route('/recipe/<int:recipe_id>')
 def recipe(recipe_id):
